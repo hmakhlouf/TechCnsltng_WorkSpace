08172c3d0b03c2a9232da2ec55950f73435d6eb0
hmakhlouf <hmakhlouf@gmail.com>
2024-02-28 23:26:11 -0600

    project1

--- PysparkTestProject/pysparktest.py
+++ PysparkTestProject/pysparktest.py
@@ -8,9 +8,9 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem") \
     .getOrCreate()
 
+
 #.config("spark.jars", "/Users/hmakhlouf/Desktop/TechCnsltng_WorkSpace/config/postgresql-42.7.2.jar") \
-
-
+ \
 # PostgreSQL's connection details
 postgres_url = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
 postgres_properties = {"user": "consultants", "password": "WelcomeItc@2022", "driver": "org.postgresql.Driver"}
@@ -18,24 +18,22 @@ postgres_properties = {"user": "consultants", "password": "WelcomeItc@2022", "dr
 # HDFS output path for full load and incremental load
 full_load_output_path = "hdfs://ec2-18-133-73-36.eu-west-2.compute.amazonaws.com:9000/tmp/USUK30/hocine/project-1/full_load_data"
 
-
-#incremental_load_output_path = "hdfs://ec2-18-133-73-36.eu-west-2.compute.amazonaws.com:9000/tmp/USUK30/hocine/project-1/incremental_load_data"
+# incremental_load_output_path = "hdfs://ec2-18-133-73-36.eu-west-2.compute.amazonaws.com:9000/tmp/USUK30/hocine/project-1/incremental_load_data"
 
 # Table name in PostgreSQL
 table_name = "car_insurance_claims"
 
 # Read data from PostgreSQL
 df_postgres = spark.read.jdbc(url=postgres_url, table=table_name, properties=postgres_properties)
+df_postgres.show()
+
 
 # Perform transformations (add your transformations here)
-#df_transformed = df_postgres.withColumn("new_column", col("existing_column") * 2)
-
+# df_transformed = df_postgres.withColumn("new_column", col("existing_column") * 2)
 # Write transformed data to HDFS
-
 # df_transformed.write.mode("overwrite").parquet(full_load_output_path)
 
-## Full Load data to hdfs
-df_postgres.write.mode("overwrite").parquet(full_load_output_path)
+## Full data  Load to hdfs
+df_postgres.write.mode("overwrite").text(full_load_output_path)
 
-# Incremental Load: Append transformed data to existing data in HDFS
-# df_transformed.write.mode("append").parquet(incremental_load_output_path)
+
