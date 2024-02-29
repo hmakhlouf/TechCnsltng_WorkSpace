e86c5a7e87212aeb41dfb170f1998fc3f4909aa9
hmakhlouf <hmakhlouf@gmail.com>
2024-02-28 23:51:59 -0600

    project1

--- PysparkTestProject/pysparktest.py
+++ PysparkTestProject/pysparktest.py
@@ -1,39 +1,28 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 
 # Initialize Spark session
-spark = SparkSession.builder \
-    .appName("CarInsuranceClaimsApp") \
-    .config("spark.hadoop.fs.defaultFS", "hdfs://ec2-18-133-73-36.eu-west-2.compute.amazonaws.com:9000") \
-    .config("spark.hadoop.fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem") \
-    .getOrCreate()
+spark = SparkSession.builder.appName("PostgresToHive").getOrCreate()
 
-
-#.config("spark.jars", "/Users/hmakhlouf/Desktop/TechCnsltng_WorkSpace/config/postgresql-42.7.2.jar") \
-
-# PostgreSQL's connection details
+# PostgreSQL connection properties
 postgres_url = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
-postgres_properties = {"user": "consultants", "password": "WelcomeItc@2022", "driver": "org.postgresql.Driver"}
+postgres_properties = {
+    "user": "consultants",
+    "password": "WelcomeItc@2022",
+    "driver": "org.postgresql.Driver",
+}
 
-# HDFS output path for full load and incremental load
-full_load_output_path = "hdfs://ec2-18-133-73-36.eu-west-2.compute.amazonaws.com:9000/tmp/USUK30/hocine/project-1/full_load_data"
-
-# incremental_load_output_path = "hdfs://ec2-18-133-73-36.eu-west-2.compute.amazonaws.com:9000/tmp/USUK30/hocine/project-1/incremental_load_data"
-
-# Table name in PostgreSQL
-table_name = "car_insurance_claims"
+# PostgreSQL table name
+postgres_table_name = "car-insurance-claims"
 
 # Read data from PostgreSQL
-df_postgres = spark.read.jdbc(url=postgres_url, table=table_name, properties=postgres_properties)
-df_postgres.show()
+df_postgres = spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties)
 
+# Hive database and table names
+hive_database_name = "USUK30"
+hive_table_name = "carInsuranceClaims"
 
-# Perform transformations (add your transformations here)
-# df_transformed = df_postgres.withColumn("new_column", col("existing_column") * 2)
-# Write transformed data to HDFS
-# df_transformed.write.mode("overwrite").parquet(full_load_output_path)
+# Save data to Hive
+df_postgres.write.mode("overwrite").saveAsTable(f"{hive_database_name}.{hive_table_name}")
 
-## Full data  Load to hdfs
-df_postgres.write.mode("overwrite") #.text(full_load_output_path)
-
-
+# Stop Spark session
+spark.stop()
