babdb2b7ccc1e8754fa1ea927c1dd417ed2e4a57
hmakhlouf <datamakhlouf@gmail.com>
2024-03-07 00:31:04 -0600

    increment update

--- project-01/pyspark/incrementalLoad.py
+++ project-01/pyspark/incrementalLoad.py
@@ -23,31 +23,50 @@ hive_table_name = "carinsuranceclaims"
 
 
 # 2. read & show new dataset from PostgresSQL:
-new_data = spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties)
-new_data.show(3)
+postgres_df = spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties)
+postgres_df.show(3)
+
+
+#-+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+-
+#-+-+--+-+--+-+--+-+--+-+-Transformations-+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--
+#-+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+-
+
+# Rename column from "ID" to "policy_number"
+postgres_df = postgres_df.withColumnRenamed("ID", "policy_number")
+
+
+#-+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+-
+#-+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+--+-+-
+
+
 
 # 3. read and show the existing_data in hive table
 existing_hive_data = spark.read.table("{}.{}".format(hive_database_name, hive_table_name))    #table("project1db.carinsuranceclaims")
 existing_hive_data.show(3)
 
-
+# 4. Determine the incremental data
 print('---------------------------------------------------------')
 print('------------------Incremental data-----------------------')
 print('---------------------------------------------------------')
-# 4. Determine the incremental data
-incremental_data_df = new_data.join(existing_hive_data.select("id"), new_data["id"] == existing_hive_data["id"], "left_anti")
+incremental_data_df = postgres_df.join(existing_hive_data.select("id"), postgres_df["id"] == existing_hive_data["id"], "left_anti")
 incremental_data_df.show()
 
+
+# counting the number of the new records added to postgres tables
 print('---------------------------------------------------------')
 print('------------------COUNTING INCREMENT RECORDS ------------')
 print('---------------------------------------------------------')
-# counting the number of the new records added to postgres tables 
-print('new records added count',incremental_data_df.count())
+new_records = incremental_data_df.count()
+print('new records added count', new_records)
 
 # 5.  Adding the incremental_data DataFrame to the existing hive table
-# write & append to the Hive table
-incremental_data_df.write.mode("append").insertInto("{}.{}".format(hive_database_name, hive_table_name))
-
+# Check if there are extra rows in PostgresSQL. if exist => # write & append to the Hive table
+if incremental_data_df.count() > 0:
+    # Append new rows to Hive table
+    incremental_data_df.write.mode("append").insertInto("{}.{}".format(hive_database_name, hive_table_name))
+    print("Appended {} new records to Hive table.".format(incremental_data_df.count()))
+else:
+    print("No new records been inserted in PostgreSQL table.")
 
 # 6. Show the new  records in hive table
 newDataHive_df = spark.sql("SELECT * FROM project1db.carinsuranceclaims cic WHERE cic.ID = 1 OR cic.ID = 2")
