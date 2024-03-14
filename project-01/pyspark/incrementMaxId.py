4d171e7f03c206d374098e7d1612644393c28b51
hmakhlouf <datamakhlouf@gmail.com>
2024-03-13 22:04:08 -0500

    project 1 update

--- project-01/pyspark/incrementMaxId.py
+++ project-01/pyspark/incrementMaxId.py
@@ -1,19 +1,51 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 
+
+
+# 1- Establish the connection to PostgresSQL and hive:
+
+# PostgresSQL connection properties
+postgres_url = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
+postgres_properties = {
+    "user": "consultants",
+    "password": "WelcomeItc@2022",
+    "driver": "org.postgresql.Driver",
+}
+postgres_table_name = "car_insurance_claims"
+
+# hive database and table names
+hive_database_name = "project1db"
+hive_table_name = "carinsuranceclaims"
+
+# 2- from the existing hive table
 spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 max_id = spark.sql("SELECT max(policy_number) FROM hocinedb.carinsuranceclaims")
 m_id = max_id.collect()[0][0]
 
-query = 'SELECT * FROM car_insurance_claims WHERE ID > ' + str(m_id)
 
-more_data = spark.read.format("jdbc") \
-    .option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb") \
-    .option("driver", "org.postgresql.Driver") \
-    .option("user", "consultants") \
-    .option("password", "WelcomeItc@2022") \
-    .option("query", query) \
-    .load()
+# 2. read & show new dataset from PostgresSQL:
+more_data = spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties)
+more_data.show(3)
+
+# 3- transformations
+
+more_data.withColumnRenamed("ID", "POLICY_NUMBER")
+# Specify the column to be modified
+columns_to_modify = ["MSTATUS", "GENDER", "EDUCATION", "OCCUPATION", "CAR_TYPE", "URBANICITY"]
+
+# Modify string values by removing "z_"
+for column in columns_to_modify:
+    more_data = more_data.withColumn(column, regexp_replace(col(column), "^z_", ""))
+
+more_data.printSchema()
+
+
+query = 'SELECT * FROM car_insurance_claims WHERE POLICY_NUMBER > ' + str(m_id)
+
+more_data = more_data.option("query", query).load()
+
+more_data.count()
 
 more_data.write.mode("append").saveAsTable("hocinedb.carinsuranceclaims")
 
